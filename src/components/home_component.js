fe0578bdf227906a36c06848b242be1c8a08d8b5
nyarlatotep <danielcrespo@cresalex.com>
2022-06-03 05:19:24 -0500

    fixing errors

--- src/components/home_component.js
+++ src/components/home_component.js
@@ -2,35 +2,43 @@ import { contentsTheme, ContentThemeContext } from '../assets/scripts/contents_t
 import { Themes } from '../assets/scripts/themes';
 const React = require( 'react' );
 
-function Introduction ( props ) {
-	return (
-		<section id='intro-section' { ...props } >
-			<article id='intro-article'>
-				<h2>
-					¿Quiénes Somos?
-				</h2>
+class Introduction extends React.Component{
+	render () {
+		let props = this.props;
+		let theme = this.context;
 
-				<p id='intro-p-one'>
-					<strong>Somos una compañía 100% mexicana</strong>, con amplia experiencia en el mercado <br />
-					de accesorios para la construcción, siendo especialistas en su comercialización.
-				</p>
+		return (
+			<section style={ theme }
+				id='intro-section' { ...props } >
+				<article id='intro-article'>
+					<h2>
+						¿Quiénes Somos?
+					</h2>
 
-				<p id='intro-p-two'>
-					Contamos con una amplia gama de productos de calidad, así como personal capacitado <br />
-					y con la disposición para atenderle de manera pronta y eficaz.
-				</p>
+					<p id='intro-p-one'>
+						<strong>Somos una compañía 100% mexicana</strong>, con amplia experiencia en el mercado <br />
+						de accesorios para la construcción, siendo especialistas en su comercialización.
+					</p>
 
-				<p id='intro-p-three'>
-					Nuestro principal objetivo es brindarle productos y servicios deprimera calidad; <br />
-					lo que nos ha permitido mantener y fortalecer relaciónes comerciales con nuestros clientes.
-					<small>
-						Confíe en la calidad y la variedad de nuestra línea de productos
-					</small>
-				</p>
-			</article>
-		</section>
-	);
+					<p id='intro-p-two'>
+						Contamos con una amplia gama de productos de calidad, así como personal capacitado <br />
+						y con la disposición para atenderle de manera pronta y eficaz.
+					</p>
+
+					<p id='intro-p-three'>
+						Nuestro principal objetivo es brindarle productos y servicios deprimera calidad; <br />
+						lo que nos ha permitido mantener y fortalecer relaciónes comerciales con nuestros clientes. <br/>
+						<small>
+							Confíe en la calidad y la variedad de nuestra línea de productos
+						</small>
+					</p>
+				</article>
+			</section>
+		);
+	};
 }
+
+Introduction.contextType = ContentThemeContext;
 
 class BodyContent extends React.Component {
 	constructor ( props ) {
@@ -45,8 +53,8 @@ class BodyContent extends React.Component {
 			this.setState( state => ( {
 				theme:
 					state.appTheme === Themes.dark
-						? contentsTheme.dark
-						: contentsTheme.light
+						? contentsTheme.light
+						: contentsTheme.dark
 			} ) );
 		};
 	}
@@ -59,7 +67,7 @@ class BodyContent extends React.Component {
 		return (
 			<>
 				<ContentThemeContext.Provider value={ this.state.theme }>
-					<Introduction style={ this.state.theme } />
+					<Introduction onChange={ this.toogleTheme } />
 				</ContentThemeContext.Provider>
 			</>
 		);
