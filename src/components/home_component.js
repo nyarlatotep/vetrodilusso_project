f7573dc3934d2787dfe08a86104c230c23c09b69
nyarlatotep <danielcrespo@cresalex.com>
2022-06-03 10:59:27 -0500

    fixing errors

--- src/components/home_component.js
+++ src/components/home_component.js
@@ -13,18 +13,15 @@ class BodyContent extends React.Component {
 					</h2>
 
 					<p className='intro-ps' id='intro-p-one'>
-							Somos una compañía 100% mexicana,
-						
+						Somos una compañía 100% mexicana,
+
 						con amplia experiencia dentro del mercado <br />
-						de accesorios para la construcción; O en otras palabras,
-						<strong>
-							somos especialistas en el comercio de dicho mercado.
-						</strong>
+						de accesorios para la construcción; O en otras palabras, <strong>
+							somos especialistas en el comercio de dicho mercado.</strong>
 					</p>
 
 					<p className='intro-ps' id='intro-p-two'>
-						Contamos con una amplia línea de productos de alta calidad, así como con
-						<strong>
+						Contamos con una amplia línea de productos de alta calidad, así como con <strong>
 							personal <br />
 							cualificado y capacitado, para brindarle atención personalizada, pronta y eficaz.
 						</strong>
