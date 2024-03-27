04f8ef0dd20a52d3464dd27fb71d55d7f0726581
nyarlatotep <danielcrespo@cresalex.com>
2024-03-27 00:55:32 +0000

    Deploy app to GitHub Pages

--- src/test/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import Header from '../components/header_component';
-
-test( 'Log testing Playground', () => {
-	render( <Header /> );
-	const linkElement = screen.logTestingPlaygroundURL( document.querySelector( '#root' ) );
-	waitFor( linkElement ).then( val => val ).catch( err => console.log( err ) );
-} );
